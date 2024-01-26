dbcd7198e7d3bb4caefdcb765cca230f08a2e7d5
pranavubarhande <pranavubarhande@gmail.com>
2024-01-26 18:46:23 +0530

    tflite version added

---
+++ modelconverter.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow import keras
+
+# Load your pre-trained model
+model = keras.models.load_model('model.h5')
+
+# Convert the model to TFLite format
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save the TFLite model to a file
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
